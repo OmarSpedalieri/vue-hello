c98de6dce262bdb6588acc55c6093098d92404f8
Omar <omarspedalieri.vfx@gmail.com>
2020-11-16 16:45:25 +0100

    test

--- js/script.js
+++ js/script.js
@@ -7,71 +7,27 @@ var app = new Vue({
 
   el: "#container",
   data:{
-
     img: "img/img1.png",
-    // img2: "img/img2.png",
-    // img3: "img/img3.jpg",
-    lv:0,
-    mex:"Scopri come ti riduce un corso Boolean",
+    mex:"Scopri come ti riduce un corso Boolean!",
   },
   methods: {
 
     changeImg: function() {
-      // var button_pressed=1;
+      var lv = 0;
 
-      if (this.lv === 0){
-        this.pics = "img/img1.png";
-        this.lv = 0;
-        console.log(this.lv);
-      }else if (this.lv === 1){
+      if (this.lv === 1){
+        this.img = "img/img1.png";
         this.lv = 1;
-        this.pics = "img/img2.png";
+        console.log(this.lv);
+      }else if (this.lv === 2){
+        this.lv = 2;
+        this.img = "img/img2.png";
           console.log(this.lv);
       }else{
-        this.lv = 2;
-        this.pics = "img/img3.jpg";
+        this.lv = 3;
+        this.img = "img/img3.jpg";
           console.log(this.lv);
       }
     }
   }
 });
-
-//
-// var app = new Vue({
-//   el: "#app",
-//   data: {
-//     nome: "nome",
-//     cognome: "cognome",
-//     mieClassi: "classe2"
-//   },
-//   methods: {
-//     changeTitle: changeTitle() {
-//       //alert("ciao dal metodo interno");
-//       if (this.mieClassi === "classe2"){
-//         this.mieClassi = "classe1";
-//       } else {
-//         this.mieClassi = "classe2";
-//       }
-//       console.log(this.cognome);
-//     }
-//   }
-// });
-//
-// var app = new Vue({
-//     el: '#root',
-//     data: {
-//         message: 'Hello World!!!!!!',
-//         mieClassi: "blue"
-//       },
-//     methods: {
-//         saluta: function () {
-//             if (this.mieClassi === "red") {
-//                 this.mieClassi = "blue";
-//             } else {
-//                 this.mieClassi = "red";
-//             }
-//             console.log(this.message);
-//         }
-//     }
-//
-//    });
